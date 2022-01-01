9a343a79a820b88c9f1c6c0ec26cb9d73c2c6c2e
yusong-offx <42.4.yusong@gmail.com>
2022-01-01 13:55:42 +0900

    crud test

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-from fastapi import FastAPI
-from database import engine
-from router.sell import models, schema
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+from router.sell import schema
+
+import models
 
 models.Base.metadata.create_all(engine)
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
+
 app = FastAPI()
 
 @app.get('/')
 def index():
 	return {'detail' : 'init project'}
 
-@app.post('/')
-def create(request: schema.test):
-	return request
+@app.get('/all')
+def get_all(db: Session = Depends(get_db)):
+	return db.query(models.Sell).all()
+
+@app.post('/test')
+def create(request:schema.test, db: Session = Depends(get_db)):
+	new_data = models.Sell(name=request.name)
+	db.add(new_data)
+	db.commit()
+	#db.refresh(new_data)
+	return 'success'
+
+@app.get('/test/{id}')
+def get_name(id: int, db: Session = Depends(get_db)):
+	return db.query(models.Sell).filter(models.Sell.id == id).all()
+
+@app.delete('/test/{id}')
+def delet_name(id: int, db: Session = Depends(get_db)):
+	for i in range(1, id+1):
+		db.query(models.Sell).filter(models.Sell.id == i).delete(synchronize_session=False)
+	db.commit()
